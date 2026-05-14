66a69ca36536a5001374002577222c1de6b4c459
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-05-14 16:16:14 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const GeowikiAPI = require('@geowiki-net/geowiki-api')
+const LeafletGeowiki = require('@geowiki-net/leaflet-geowiki/minimal')
+
+var options = {}
+
+// Create Leaflet map object
+var map = L.map('map').setView([ 48.21, 16.37], 12)
+
+// Show OSM map background
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// create link to overpass database (this could be a file too, e.g. data.osm or data.osm.bz2)
+const geowikiAPI = new GeowikiAPI('https://radnetz-dashboard.radlobby.at/bauprojekte?type=1&title=&jahr=&field_bilanzjahr_value=&tags_id=Stadtentwicklungsplan+%2F+STEP+2035+%281538%29&field_bezirk_target_id=All&field_status_target_id=All&field_verschoben=All&field_radlobby_prioritaet_value=All&field_netz_id=All&field_route_target_id=All')
+
+// Initialize Geowiki viewer
+var geowiki = new LeafletGeowiki({
+  geowikiAPI,
+  styleFile: 'bauprojekte.yaml',
+}).addTo(map)
+
+// We could also add the map info with this command
+//geowiki.setOption('info', { dom: document.getElementById('info') })
